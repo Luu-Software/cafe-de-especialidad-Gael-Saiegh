846e6585b5504b8d580dbe8b5f757a14df385155
Gael-Saiegh <gaesaiegh@gmail.com>
2026-05-27 14:26:00 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,17 @@ import { preguntar } from './lib/consola.ts';
 let totalTexto: string = preguntar('Total de la cuenta: ');
 let esEstudiante: string = preguntar('¿Son estudiantes de ORT? si/no: ');
 let personasTexto: string = preguntar('Dividir la cuenta entre: ');
+let 
 
 let totalPorPersona: number = 0; // COMPLETAR
 console.log(`Total por persona: $${totalPorPersona}`);
+ 
+if (esEstudiante==='si') { 
+    Number(totalTexto)
+    Number (personasTexto)
+    Number (totalTexto)
+ (totalTexto / personasTexto) .090 
+}
+
+
+Total por persona: $<monto>
